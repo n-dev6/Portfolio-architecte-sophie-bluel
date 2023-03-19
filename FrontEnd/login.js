2b768dc2f5722549e8f6f53ca635f4c49aaa9e34
nory <nory.brakhlia@gmail.com>
2023-03-19 01:41:36 +0100

    ajout partiel de la modale

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -1,30 +1,39 @@
 document.addEventListener('DOMContentLoaded', () => {
-const loginForm = document.querySelector('#login-form');
-const loginEndPoint = 'http://localhost:5678/api/users/login'
+  const loginForm = document.querySelector('#login-form');
+  const loginEndPoint = 'http://localhost:5678/api/users/login';
+  let loggedIn = false;
+  const adminToolbar = document.querySelector(".admin-editor-toolbar");
 
-loginForm.addEventListener('submit', (event) => {
-  event.preventDefault(); // Empêche la soumission du formulaire
+  loginForm.addEventListener('submit', (event) => {
+    event.preventDefault(); // Empêche la soumission du formulaire
 
-  const username = loginForm.elements['email'].value;
-  const password = loginForm.elements['password'].value;
+    const username = loginForm.elements['email'].value;
+    const password = loginForm.elements['password'].value;
 
-  fetch(loginEndPoint, {
-    method: 'GET',
-    headers: {
-      'Authorization': `Basic ${btoa(username + ':' + password)}`
-    }
+    fetch(loginEndPoint, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        email: username,
+        password: password
+      })
+    })
+    .then(response => {
+      if (response.status === 200) {
+        loggedIn = true;
+        console.log(loggedIn);
+        window.location.replace('index.html');
+      } else {
+        const errorMessage = document.createElement('p');
+        errorMessage.textContent = 'Erreur dans l’identifiant ou le mot de passe';
+        loginForm.appendChild(errorMessage); // Ajoute le message d'erreur au formulaire
+        loggedIn = false;
+      }
   })
-  .then(response => {
-    if (response.status === 200) {
-      window.location.href = 'index.html'; // Redirige vers la page d'édition
-    } else {
-      const errorMessage = document.createElement('p');
-      errorMessage.textContent = 'Erreur dans l’identifiant ou le mot de passe';
-      loginForm.appendChild(errorMessage); // Ajoute le message d'erreur au formulaire
-    }
-  })
-  .catch(error => {
-    console.error(error);
+    .catch(error => {
+      console.error(error);
+    });
   });
-});
 });
