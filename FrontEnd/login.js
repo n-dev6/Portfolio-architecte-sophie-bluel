e887842d881dfe41f83712232515c99141c19cf0
nory <nory.brakhlia@gmail.com>
2023-03-21 00:51:01 +0100

    stockage du token / login-logout boutton implementé

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -3,37 +3,83 @@ document.addEventListener('DOMContentLoaded', () => {
   const loginEndPoint = 'http://localhost:5678/api/users/login';
   let loggedIn = false;
   const adminToolbar = document.querySelector(".admin-editor-toolbar");
+  const loginParagraphe = document.querySelector(".login-head-paragraphe");
+  const logoutParagraphe = document.querySelector(".logout-head-paragraphe");
 
-  loginForm.addEventListener('submit', (event) => {
-    event.preventDefault(); // Empêche la soumission du formulaire
+  if (window.location.pathname.includes('login.html')) {
+    loginForm.addEventListener('submit', (event) => {
+      event.preventDefault(); // Empêche la soumission du formulaire
 
-    const username = loginForm.elements['email'].value;
-    const password = loginForm.elements['password'].value;
+      const username = loginForm.elements['email'].value;
+      const password = loginForm.elements['password'].value;
 
-    fetch(loginEndPoint, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        email: username,
-        password: password
+      fetch(loginEndPoint, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          email: username,
+          password: password
+        })
       })
-    })
-    .then(response => {
-      if (response.status === 200) {
-        loggedIn = true;
+      .then(response => {
+        if (response.status === 200) {
+          return response.json(); // extraire le corps de la réponse sous forme d'objet JavaScript
+        } else {
+          throw new Error('Erreur dans l’identifiant ou le mot de passe');
+        }
+      })
+      .then(data => {
+        const token = data.token; // extraire le token de l'objet JavaScript retourné
+        sessionStorage.setItem('token', token);
+        console.log(token);
         console.log(loggedIn);
-        window.location.replace('index.html');
-      } else {
-        const errorMessage = document.createElement('p');
-        errorMessage.textContent = 'Erreur dans l’identifiant ou le mot de passe';
-        loginForm.appendChild(errorMessage); // Ajoute le message d'erreur au formulaire
-        loggedIn = false;
-      }
-  })
-    .catch(error => {
-      console.error(error);
+        window.location.href = 'index.html';
+        loggedIn = true;
+      })
+      .catch(error => {
+        console.error(error);
+      });
     });
-  });
+  } 
+  else {
+    const token = sessionStorage.getItem('token');
+    const buttonContainer = document.querySelector('.button');
+    const modifyFigure = document.querySelector('.modify-figure-text');
+    const loggedModifyText = document.querySelector(".logged-modify-text")
+    const loggedModifyIcon = document.querySelector('.logo-modify-login');
+  
+    if (token) {
+      loginParagraphe.classList.add('loggedIn')
+      logoutParagraphe.classList.add('loggedIn')
+      adminToolbar.classList.add('loggedIn');
+      modifyFigure.classList.add('loggedIn');
+      loggedModifyText.classList.add('loggedIn');
+      loggedModifyIcon.classList.add('loggedIn');
+      
+      buttonContainer.classList.add('invisible');
+      console.log(token);
+      console.log("logged");
+      
+    } else {
+      
+      loginParagraphe.classList.remove('loggedIn')
+      logoutParagraphe.classList.remove('loggedIn')
+    }
+  }
+
+  if (logoutParagraphe) {
+    logoutParagraphe.addEventListener('click', (event) => {
+      event.preventDefault();
+  
+      // Supprimer le token de la session storage
+      sessionStorage.removeItem('token');
+  
+      // Rediriger l'utilisateur vers la page de connexion
+      window.location.href = 'login.html';
+    });
+  }
 });
+
+
--- FrontEnd/main.js
+++ FrontEnd/main.js
@@ -2,12 +2,11 @@
 const workEndPoint = "http://localhost:5678/api/works";
 const categorieEndPoint = "http://localhost:5678/api/categories";
 
-
 document.addEventListener("DOMContentLoaded", () => {
+
   fetchWorks();
   const buttonAll = document.querySelector('.allButton');
   buttonAll.addEventListener('click', () => fetchWorks());
-  
   fetch(categorieEndPoint)
   .then(response => response.json())
   .then(categories => {
@@ -29,13 +28,17 @@ document.addEventListener("DOMContentLoaded", () => {
         // filtrer les projets en fonction de la catégorie sélectionnée
         // (code à implémenter)
       });
+    
       
       // ajouter le bouton à l'élément parent
       buttonContainer.appendChild(button);
     });
+  
   })
   .catch(error => console.error(error));
+
 });
+
 
 
 // récupérer les données de l'API
