dbbdc387e02726c34006fb7c04f1e37df99c743c
nory <nory.brakhlia@gmail.com>
2023-03-28 06:14:59 +0200

    Update-modal-issue

--- FrontEnd/modal.js
+++ FrontEnd/modal.js
@@ -18,6 +18,12 @@ const categorieEndPoint = "http://localhost:5678/api/categories";
     const fileInput = document.querySelector('#image');
     const submitProjet = document.querySelector(".submit-projet")
     const hidingPreviewPhoto = document.querySelector(".add-picture-hiding")
+    let categorieAlreadyLoad = false
+    const previewImage = document.querySelector('#preview-image');
+    const successBtn = document.querySelector("#btn-success");
+    const errorBtn = document.querySelector("#btn-error");
+
+
 
 
 
@@ -45,7 +51,7 @@ const categorieEndPoint = "http://localhost:5678/api/categories";
         img.src = event.target.result;
         // Remplacer l'icône par l'image prévisualisée
         const icon = addPicture.querySelector("i");
-        icon.replaceWith(img);
+     
       });
       reader.readAsDataURL(file);
     });
@@ -56,12 +62,20 @@ const categorieEndPoint = "http://localhost:5678/api/categories";
       updatePreviewImage(document.querySelector('#preview-image'));
     });
     
-    
+    //attention les categories ce charge deux fois
 
     returnContent.addEventListener('click', () =>{
       modal.style.display = 'block'
       modalContent.style.display ='block';
       modalPhoto.style.display ='none'
+      updateModal()
+      form = document.querySelector('#add-projet-form')
+      form.reset()
+      removePreviewImage(previewImage);
+      hidingPreviewPhoto.style.display = 'flex';
+      successBtn.style.display='none'
+
+    
     });
 
 
@@ -70,19 +84,23 @@ const categorieEndPoint = "http://localhost:5678/api/categories";
       modalContent.style.display ='block';
       modalPhoto.style.display ='none'
       fetchWorks();
+      successBtn.style.display='none'
+      errorBtn.style.display='none'
     });
 
     close.addEventListener('click', () => {
-      modal.style.display = 'none';  
+      modal.style.display = 'none';
+      successBtn.style.display='none'
 
     });
     
     addPhoto.addEventListener('click', (event) =>{
-      
       modalContent.style.display ='none'
       modalPhoto.style.display ='flex'
+      if (!categorieAlreadyLoad){
       loadCategorieModal()
-
+      categorieAlreadyLoad = true
+      }
     });
 
     closeContent.addEventListener('click',() =>{
@@ -91,7 +109,35 @@ const categorieEndPoint = "http://localhost:5678/api/categories";
      });
 
 
+  }
 
+  //Fonctions
+  function removePreviewImage(img) {
+    img.src = '';
+  }
+
+  function updateModal(categoryId) {
+    fetchWorks(categoryId);
+  }
+
+  function deleteWork(workId) {
+    fetch("http://localhost:5678/api/works/"+ '/' + workId, {
+      method: 'DELETE',
+      headers: {
+        'Authorization': `Bearer ${token}`
+      }
+      
+    })
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      
+      // Mettre à jour l'affichage ou faire d'autres traitements en cas de succès
+    })
+    .catch(error => {
+      console.error(error);
+      // Gérer les erreurs
+    });
   }
   
 
@@ -111,16 +157,28 @@ const categorieEndPoint = "http://localhost:5678/api/categories";
     
     // Créer un objet FormData pour envoyer le fichier à l'API
     const formData = new FormData();
+    console.log('form-upload')
     formData.append('image', file);
+    console.log('image-upload')
     formData.append('title', title);
+    console.log('title-upload')
     if (selectedCategory) {
       formData.append('category', selectedCategory);
     }
-    
+    console.log('click-upload')
     // Envoyer la requête POST à l'API
+    sessionStorage.getItem(token)
+    console.log(token)
     
+    const headers = new Headers();
+
+    // Ajouter le token à l'en-tête
+    headers.append('Authorization', `Bearer ${token}`);
+    
+    // Envoyer la requête POST à l'API avec les en-têtes
     fetch("http://localhost:5678/api/works", {
       method: 'POST',
+      headers: headers,
       body: formData,
     })
       .then(response => response.json())
@@ -133,6 +191,8 @@ const categorieEndPoint = "http://localhost:5678/api/categories";
         console.error(error);
         alert('Une erreur s\'est produite lors de l\'envoi de la photo. Veuillez réessayer plus tard.');
       });
+      const successBtn = document.querySelector("#btn-success");
+      successBtn.style.display='block'
   }
 
 //Mehode pour recupérer les données de categorie a l'api
@@ -146,10 +206,11 @@ function loadCategorieModal(){
       option.value = category.id;
       option.textContent = category.name;
       select.appendChild(option);
+      categorieAlreadyLoad = true
     });
   });
   }
-
+  // affiche l'image 
   function updatePreviewImage(img) {
     const fileInput = document.querySelector('#image');
     
@@ -166,23 +227,37 @@ function loadCategorieModal(){
     });
   }
 
-function fetchWorks(categoryId){
-  fetch(workEndPoint)
-  .then(response => response.json())
-  .then(works => {
-    const filteredWorks = categoryId ? works.filter(work => work.categoryId == categoryId): works;
-    const galleryModal = document.querySelector(".gallery-modal")
-    removeAllChildNodes(galleryModal);
-    // ajouter la figure à la page
-    
-    filteredWorks.forEach(work => {
-      // transformation du work en figure html
-     
-      const figure = createFigure(work);
-      galleryModal.appendChild(figure);
-    });
-  });
-}
+
+
+
+  function fetchWorks(categoryId){
+    fetch(workEndPoint)
+    .then(response => response.json())
+    .then(works => {
+      const filteredWorks = categoryId ? works.filter(work => work.categoryId == categoryId): works;
+      const galleryModal = document.querySelector(".gallery-modal")
+      removeAllChildNodes(galleryModal);
+      // ajouter la figure à la page   
+      filteredWorks.forEach(work => {
+        // transformation du work en figure html   
+        const figure = createFigure(work);     
+        galleryModal.appendChild(figure);
+        const deleteIcon = document.createElement('i')
+        deleteIcon.className = 'fa fa-trash'
+        deleteIcon.style.cursor = 'pointer'; // ajout d'un style cursor pointer pour indiquer que l'icône est cliquable
+        deleteIcon.addEventListener('click', () => {
+          // supprimer le travail associé au clic sur l'icône de la corbeille
+          deleteWork(work.id);
+          // supprimer la figure correspondante de la page
+          galleryModal.removeChild(figure);
+          updateModal();
+          const successBtn = document.querySelector("btn-success")
+          successBtn.style.display='block'
+        });
+        figure.appendChild(deleteIcon);
+      });
+    });  
+  }
 
 function createFigure(work){
   const figure = document.createElement('figure');
